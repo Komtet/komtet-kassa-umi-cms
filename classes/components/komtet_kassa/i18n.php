788f8442a0e67e16b5f0f959c46518bac97d3d37
Юрий Никитин <yura.motmom@gmail.com>
2020-06-16 15:32:17 +0300

    переименование полей

--- classes/components/komtet_kassa/i18n.php
+++ classes/components/komtet_kassa/i18n.php
@@ -5,9 +5,9 @@
 	$i18n = [
 		'module-komtet_kassa'				=> 'КОМТЕТ Касса',
 		'header-komtet_kassa-config_module'	=> 'Настройка модуля',
-		'option-shop_id'					=> 'Shop ID',
-        'option-queue_id'					=> 'Queue ID',
-        'option-shop_secret'				=> 'Shop Secret',
+		'option-shop_id'					=> 'ID магазина',
+        'option-queue_id'					=> 'ID очереди',
+        'option-shop_secret'				=> 'Секретный ключ магазина',
         'option-sno'						=> 'СНО',
         'option-vat'						=> 'Налоговая ставка',
         'option-is_print_check'				=> 'Печатать ли чек',
