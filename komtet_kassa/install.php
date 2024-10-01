73391a4d51d7439c29fa18310eae943dfc6f3bf8
Алексей Ефремчев <efremchev@gmail.com>
2024-10-01 10:49:03 +0300

    Скрытие ЕНВД из списка СНО

--- komtet_kassa/install.php
+++ komtet_kassa/install.php
@@ -4,7 +4,7 @@
  * @var array $INFO реестр модуля
  */
 $INFO = [
-    'name'                  => 'komtet_kassa', // Имя модуля
+    'name' => 'komtet_kassa', // Имя модуля
     'config' => '1', // У модуля есть настройки
     'default_method_admin' => 'config_module',
 ];
@@ -21,7 +21,7 @@ $COMPONENTS = [
     './classes/components/komtet_kassa/class.php',
     './classes/components/komtet_kassa/customAdmin.php',
     './classes/components/komtet_kassa/customMacros.php',
-    './classes/components/komtet_kassa/i18n.php', // языковые константы для Административной часи
+    './classes/components/komtet_kassa/i18n.php', // языковые константы для Административной части
     './classes/components/komtet_kassa/install.php',
     './classes/components/komtet_kassa/lang.php', //языковые константы для Клиентской части
     './classes/components/komtet_kassa/macros.php',
